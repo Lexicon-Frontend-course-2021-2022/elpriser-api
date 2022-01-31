c56968e5f0497739b626478253fd121cc1307466
Thomas Hellström <rel@xed.se>
2022-01-31 14:55:39 +0100

    Cleaning up

--- index.js
+++ index.js
@@ -8,86 +8,66 @@ const dayjs = require('dayjs');
 
 app.use(express.json());
 
-const baseUrl = 'http://www.nordpoolspot.com/api/marketdata/page/29'
-
 // Root url '/'
 app.get('/today', (req, res) => {
 
   const now = new Date(Date.now());
 
-  const endDate = dayjs(now).format('DD-MM-YYYY');
-  const url = baseUrl + '?currency=SEK,SEK,SEK&endDate=' + endDate
   const api_data = {
-    date: dayjs(now).format('YYYY-MM-DD'),
     areas: {
-      SE1: {
-        min: null,
-        max: null,
-        mean: null,
-        hourly: []
-      },
-      SE2: {
-        min: null,
-        max: null,
-        mean: null,
-        hourly: []
-      },
-      SE3: {
-        min: null,
-        max: null,
-        mean: null,
-        hourly: []
-      },
-      SE4: {
-        min: null,
-        max: null,
-        mean: null,
-        hourly: []
-      },
+      SE1: { hourly: [] },
+      SE2: { hourly: [] },
+      SE3: { hourly: [] },
+      SE4: { hourly: [] },
     }
   };
 
-  Request.get(url, (error, result, body) => {
+  const endDate = dayjs(now).format('DD-MM-YYYY');
+  Request.get(
+    `http://www.nordpoolspot.com/api/marketdata/page/29?currency=SEK,SEK,SEK&endDate=${endDate}`,
+    (error, result, body) => {
 
-    if (error) {
-      throw error;
-    }
-
-    const data = JSON.parse(body)["data"]["Rows"];
-
-    const out = [];
-    let hour = 0;
-
-    data.map(item => {
-
-      item.Columns.map(area => {
-
-        const value = (parseFloat(area.Value.replace(',', '.').replace(' ', '')) / 10).toFixed(2)
-        if (!item.IsExtraRow) {
-          api_data.areas[area.Name].hourly.push(value);
-        } else {
-          if (item.Name === 'Max') {
-            api_data.areas[area.Name].max = value;
-          }
-          if (item.Name === 'Min') {
-            api_data.areas[area.Name].min = value;
-          }
-          if (item.Name === 'Average') {
-            api_data.areas[area.Name].mean = value;
-          }
-        }
-      })
-
-    });
-    res.json({
-      result: 'success',
-      data: {
-        hours: api_data.areas.SE1.hourly.length,
-        ...api_data
+      if (error) {
+        throw error;
       }
-    });
 
-  })
+      const data = JSON.parse(body)["data"]["Rows"];
+
+      const out = [];
+      let hour = 0;
+
+      data.map(item => {
+
+        item.Columns.map(area => {
+
+          const value = (parseFloat(area.Value.replace(',', '.').replace(' ', '')) / 10).toFixed(2)
+          if (!item.IsExtraRow) {
+            api_data.areas[area.Name].hourly.push(value);
+          } else {
+            if (item.Name === 'Max') {
+              api_data.areas[area.Name].max = value;
+            }
+            if (item.Name === 'Min') {
+              api_data.areas[area.Name].min = value;
+            }
+            if (item.Name === 'Average') {
+              api_data.areas[area.Name].mean = value;
+            }
+          }
+        })
+
+      });
+      res.json({
+        result: 'success',
+        data: {
+          date: dayjs(now).format('YYYY-MM-DD'),
+          hours: api_data.areas.SE1.hourly.length,
+          unit: 'öre/kWh',
+          ...api_data
+        }
+      });
+
+    })
 });
 
 
